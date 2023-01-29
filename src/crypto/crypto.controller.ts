9b4d60df58579c5e398567520202efdd3b3b5daf
Wisarut Promklai <wisarut.p@teohong.com>
2023-01-29 12:16:28 +0700

    Update swagger

--- src/crypto/crypto.controller.ts
+++ src/crypto/crypto.controller.ts
@@ -6,7 +6,7 @@ import {
   UseInterceptors,
   CacheInterceptor,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HTTPResponse } from 'src/common/http/response';
 import { Result } from 'src/common/result/result';
 import { CryptoService } from './crypto.service';
@@ -17,8 +17,29 @@ import { CryptoService } from './crypto.service';
 export class CryptoController {
   constructor(private readonly cryptoService: CryptoService) {}
 
+  @ApiQuery({
+    name: 'symbols',
+    required: false,
+    type: 'string',
+    description: 'a symbol to get current price in usd.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Get crypto price successfully.',
+    schema: {
+      example: {
+        statusCode: 200,
+        message: 'Get crypto price successfully.',
+        timestamp: '2023-01-28T14:15:00.008Z',
+        data: {
+          symbol: 'ETH',
+          currentPrice: 1573.56,
+        },
+      },
+    },
+  })
   @Get('price')
-  async getCryptoPrice(@Query('symbol') symbol) {
+  async getCryptoPrice(@Query('symbols') symbol) {
     const result = await this.cryptoService.getCryptoPrice(
       symbol?.toUpperCase(),
     );
@@ -40,13 +61,307 @@ export class CryptoController {
     return responseError;
   }
 
+  @ApiQuery({
+    name: 'query',
+    required: false,
+    type: 'string',
+    description: 'query string for search.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Search crypto list successfully.',
+    schema: {
+      example: {
+        statusCode: 200,
+        message: 'Search crypto list successfully.',
+        timestamp: '2023-01-29T05:08:12.767Z',
+        data: [
+          {
+            id: 'solana',
+            name: 'Solana',
+            api_symbol: 'solana',
+            symbol: 'SOL',
+            market_cap_rank: 12,
+            thumb:
+              'https://assets.coingecko.com/coins/images/4128/thumb/solana.png',
+            large:
+              'https://assets.coingecko.com/coins/images/4128/large/solana.png',
+          },
+          {
+            id: 'msol',
+            name: 'Marinade staked SOL',
+            api_symbol: 'msol',
+            symbol: 'MSOL',
+            market_cap_rank: 187,
+            thumb:
+              'https://assets.coingecko.com/coins/images/17752/thumb/mSOL.png',
+            large:
+              'https://assets.coingecko.com/coins/images/17752/large/mSOL.png',
+          },
+          {
+            id: 'lido-staked-sol',
+            name: 'Lido Staked SOL',
+            api_symbol: 'lido-staked-sol',
+            symbol: 'STSOL',
+            market_cap_rank: 269,
+            thumb:
+              'https://assets.coingecko.com/coins/images/18369/thumb/logo_-_2021-09-15T100934.765.png',
+            large:
+              'https://assets.coingecko.com/coins/images/18369/large/logo_-_2021-09-15T100934.765.png',
+          },
+          {
+            id: 'solo-coin',
+            name: 'Sologenic',
+            api_symbol: 'solo-coin',
+            symbol: 'SOLO',
+            market_cap_rank: 372,
+            thumb:
+              'https://assets.coingecko.com/coins/images/10771/thumb/solo.png',
+            large:
+              'https://assets.coingecko.com/coins/images/10771/large/solo.png',
+          },
+          {
+            id: 'solve-care',
+            name: 'SOLVE',
+            api_symbol: 'solve-care',
+            symbol: 'SOLVE',
+            market_cap_rank: 718,
+            thumb:
+              'https://assets.coingecko.com/coins/images/1768/thumb/Solve.Token_logo_200_200_wiyhout_BG.png',
+            large:
+              'https://assets.coingecko.com/coins/images/1768/large/Solve.Token_logo_200_200_wiyhout_BG.png',
+          },
+          {
+            id: 'green-satoshi-token',
+            name: 'STEPN Green Satoshi Token on Solana',
+            api_symbol: 'green-satoshi-token',
+            symbol: 'GST-SOL',
+            market_cap_rank: 788,
+            thumb:
+              'https://assets.coingecko.com/coins/images/21841/thumb/gst.png',
+            large:
+              'https://assets.coingecko.com/coins/images/21841/large/gst.png',
+          },
+          {
+            id: 'solend',
+            name: 'Solend',
+            api_symbol: 'solend',
+            symbol: 'SLND',
+            market_cap_rank: 888,
+            thumb:
+              'https://assets.coingecko.com/coins/images/19573/thumb/i6AMOwun_400x400.jpg',
+            large:
+              'https://assets.coingecko.com/coins/images/19573/large/i6AMOwun_400x400.jpg',
+          },
+          {
+            id: 'socean-staked-sol',
+            name: 'Socean Staked Sol',
+            api_symbol: 'socean-staked-sol',
+            symbol: 'SCNSOL',
+            market_cap_rank: 1133,
+            thumb:
+              'https://assets.coingecko.com/coins/images/18468/thumb/biOTzfxE_400x400.png',
+            large:
+              'https://assets.coingecko.com/coins/images/18468/large/biOTzfxE_400x400.png',
+          },
+          {
+            id: 'solcial',
+            name: 'Solcial',
+            api_symbol: 'solcial',
+            symbol: 'SLCL',
+            market_cap_rank: 1186,
+            thumb:
+              'https://assets.coingecko.com/coins/images/24583/thumb/1_N9szP0_400x400.jpg',
+            large:
+              'https://assets.coingecko.com/coins/images/24583/large/1_N9szP0_400x400.jpg',
+          },
+          {
+            id: 'solanium',
+            name: 'Solanium',
+            api_symbol: 'solanium',
+            symbol: 'SLIM',
+            market_cap_rank: 1214,
+            thumb:
+              'https://assets.coingecko.com/coins/images/15816/thumb/logo_cg.png',
+            large:
+              'https://assets.coingecko.com/coins/images/15816/large/logo_cg.png',
+          },
+          {
+            id: 'trisolaris',
+            name: 'Trisolaris',
+            api_symbol: 'trisolaris',
+            symbol: 'TRI',
+            market_cap_rank: 2090,
+            thumb:
+              'https://assets.coingecko.com/coins/images/20607/thumb/logo_-_2021-11-19T104946.772.png',
+            large:
+              'https://assets.coingecko.com/coins/images/20607/large/logo_-_2021-11-19T104946.772.png',
+          },
+          {
+            id: 'solrise-finance',
+            name: 'Solrise Finance',
+            api_symbol: 'solrise-finance',
+            symbol: 'SLRS',
+            market_cap_rank: 2097,
+            thumb:
+              'https://assets.coingecko.com/coins/images/15762/thumb/9989.png',
+            large:
+              'https://assets.coingecko.com/coins/images/15762/large/9989.png',
+          },
+          {
+            id: 'solchicks-token',
+            name: 'SolChicks',
+            api_symbol: 'solchicks-token',
+            symbol: 'CHICKS',
+            market_cap_rank: 2242,
+            thumb:
+              'https://assets.coingecko.com/coins/images/20978/thumb/chicks.png',
+            large:
+              'https://assets.coingecko.com/coins/images/20978/large/chicks.png',
+          },
+          {
+            id: 'solace',
+            name: 'SOLACE',
+            api_symbol: 'solace',
+            symbol: 'SOLACE',
+            market_cap_rank: 2295,
+            thumb:
+              'https://assets.coingecko.com/coins/images/21277/thumb/IMG_6599.png',
+            large:
+              'https://assets.coingecko.com/coins/images/21277/large/IMG_6599.png',
+          },
+          {
+            id: 'solarbeam',
+            name: 'Solarbeam',
+            api_symbol: 'solarbeam',
+            symbol: 'SOLAR',
+            market_cap_rank: 2388,
+            thumb:
+              'https://assets.coingecko.com/coins/images/18260/thumb/solarbeamlogo.png',
+            large:
+              'https://assets.coingecko.com/coins/images/18260/large/solarbeamlogo.png',
+          },
+          {
+            id: 'solape-token',
+            name: 'SOLAPE',
+            api_symbol: 'solape-token',
+            symbol: 'SOLAPE',
+            market_cap_rank: 2483,
+            thumb:
+              'https://assets.coingecko.com/coins/images/16930/thumb/128px-coin.png',
+            large:
+              'https://assets.coingecko.com/coins/images/16930/large/128px-coin.png',
+          },
+          {
+            id: 'soldex',
+            name: 'Soldex',
+            api_symbol: 'soldex',
+            symbol: 'SOLX',
+            market_cap_rank: 2518,
+            thumb:
+              'https://assets.coingecko.com/coins/images/21362/thumb/output-onlinepngtools_%2811%29.png',
+            large:
+              'https://assets.coingecko.com/coins/images/21362/large/output-onlinepngtools_%2811%29.png',
+          },
+          {
+            id: 'apollon-limassol',
+            name: 'Apollon Limassol Fan Token',
+            api_symbol: 'apollon-limassol',
+            symbol: 'APL',
+            market_cap_rank: 2545,
+            thumb:
+              'https://assets.coingecko.com/coins/images/13054/thumb/Apollon-10_%28NEW%29.jpg',
+            large:
+              'https://assets.coingecko.com/coins/images/13054/large/Apollon-10_%28NEW%29.jpg',
+          },
+          {
+            id: 'soldoge',
+            name: 'SolDoge',
+            api_symbol: 'soldoge',
+            symbol: 'SDOGE',
+            market_cap_rank: 2622,
+            thumb:
+              'https://assets.coingecko.com/coins/images/19746/thumb/2L4aX1r.png',
+            large:
+              'https://assets.coingecko.com/coins/images/19746/large/2L4aX1r.png',
+          },
+          {
+            id: 'solanax',
+            name: 'Solanax',
+            api_symbol: 'solanax',
+            symbol: 'SOLD',
+            market_cap_rank: 2709,
+            thumb:
+              'https://assets.coingecko.com/coins/images/17634/thumb/sold.png',
+            large:
+              'https://assets.coingecko.com/coins/images/17634/large/sold.png',
+          },
+          {
+            id: 'solvent',
+            name: 'Solvent',
+            api_symbol: 'solvent',
+            symbol: 'SVT',
+            market_cap_rank: 2877,
+            thumb:
+              'https://assets.coingecko.com/coins/images/22387/thumb/svt.png',
+            large:
+              'https://assets.coingecko.com/coins/images/22387/large/svt.png',
+          },
+          {
+            id: 'solidex',
+            name: 'Solidex',
+            api_symbol: 'solidex',
+            symbol: 'SEX',
+            market_cap_rank: 3040,
+            thumb:
+              'https://assets.coingecko.com/coins/images/23855/thumb/photo_2022-02-23_09.01.21.jpeg',
+            large:
+              'https://assets.coingecko.com/coins/images/23855/large/photo_2022-02-23_09.01.21.jpeg',
+          },
+          {
+            id: 'parasol-finance',
+            name: 'Parasol Finance',
+            api_symbol: 'parasol-finance',
+            symbol: 'PSOL',
+            market_cap_rank: 3120,
+            thumb:
+              'https://assets.coingecko.com/coins/images/21551/thumb/icon.png',
+            large:
+              'https://assets.coingecko.com/coins/images/21551/large/icon.png',
+          },
+          {
+            id: 'solberg',
+            name: 'Solberg',
+            api_symbol: 'solberg',
+            symbol: 'SLB',
+            market_cap_rank: 3165,
+            thumb:
+              'https://assets.coingecko.com/coins/images/18918/thumb/logo_%2822%29.png',
+            large:
+              'https://assets.coingecko.com/coins/images/18918/large/logo_%2822%29.png',
+          },
+          {
+            id: 'x-consoles',
+            name: 'X-Consoles',
+            api_symbol: 'x-consoles',
+            symbol: 'GAME',
+            market_cap_rank: 3274,
+            thumb:
+              'https://assets.coingecko.com/coins/images/21616/thumb/photo_2021-12-25_01-03-14.jpg',
+            large:
+              'https://assets.coingecko.com/coins/images/21616/large/photo_2021-12-25_01-03-14.jpg',
+          },
+        ],
+      },
+    },
+  })
   @Get('search')
   async getCryptoList(@Query('query') symbol) {
     const result = await this.cryptoService.getCryptoList(symbol);
     if (result instanceof Result) {
       const response: HTTPResponse = {
         statusCode: HttpStatus.OK,
-        message: 'Get crypto list successfully.',
+        message: 'Search crypto list successfully.',
         timestamp: new Date().toISOString(),
         data: result.data,
       };
--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -7,7 +7,7 @@ import {
   HttpStatus,
   HttpException,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HTTPResponse } from 'src/common/http/response';
 import { Result } from 'src/common/result/result';
 import { StockService } from './stock.service';
@@ -18,6 +18,35 @@ import { StockService } from './stock.service';
 export class StockController {
   constructor(private readonly stockService: StockService) {}
 
+  @ApiQuery({
+    name: 'symbol',
+    required: false,
+    type: 'string',
+    description: 'a symbol to get current price in usd.',
+  })
+  @ApiQuery({
+    name: 'companyName',
+    required: false,
+    type: 'string',
+    description: 'get current price by seach company name.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Get stock price successfully.',
+    schema: {
+      example: {
+        statusCode: 200,
+        message: 'Get stock price successfully.',
+        timestamp: '2023-01-29T05:05:46.505Z',
+        data: [
+          {
+            currentPrice: 145.93,
+            symbol: 'AAPL',
+          },
+        ],
+      },
+    },
+  })
   @Get('price')
   async getStockPrice(
     @Query('symbols') symbol,
@@ -57,6 +86,238 @@ export class StockController {
     return responseError;
   }
 
+  @ApiQuery({
+    name: 'query',
+    required: false,
+    type: 'string',
+    description: 'query string for search.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Search stock list successfully.',
+    schema: {
+      example: {
+        statusCode: 200,
+        message: 'Search stock list successfully.',
+        timestamp: '2023-01-29T05:06:30.133Z',
+        data: {
+          explains: [],
+          count: 11,
+          quotes: [
+            {
+              exchange: 'NMS',
+              shortname: 'Apple Inc.',
+              quoteType: 'EQUITY',
+              symbol: 'AAPL',
+              index: 'quotes',
+              score: 373859,
+              typeDisp: 'Equity',
+              longname: 'Apple Inc.',
+              exchDisp: 'NASDAQ',
+              sector: 'Technology',
+              industry: 'Consumer Electronics',
+              dispSecIndFlag: true,
+              isYahooFinance: true,
+            },
+            {
+              exchange: 'NYQ',
+              shortname: 'Apple Hospitality REIT, Inc.',
+              quoteType: 'EQUITY',
+              symbol: 'APLE',
+              index: 'quotes',
+              score: 21748,
+              typeDisp: 'Equity',
+              longname: 'Apple Hospitality REIT, Inc.',
+              exchDisp: 'NYSE',
+              sector: 'Real Estate',
+              industry: 'REIT—Hotel & Motel',
+              isYahooFinance: true,
+            },
+            {
+              exchange: 'NEO',
+              shortname: 'APPLE CDR (CAD HEDGED)',
+              quoteType: 'EQUITY',
+              symbol: 'AAPL.NE',
+              index: 'quotes',
+              score: 20534,
+              typeDisp: 'Equity',
+              longname: 'Apple Inc.',
+              exchDisp: 'NEO',
+              sector: 'Technology',
+              industry: 'Consumer Electronics',
+              isYahooFinance: true,
+            },
+            {
+              exchange: 'GER',
+              shortname: 'APPLE INC',
+              quoteType: 'EQUITY',
+              symbol: 'APC.DE',
+              index: 'quotes',
+              score: 20108,
+              typeDisp: 'Equity',
+              longname: 'Apple Inc.',
+              exchDisp: 'XETRA',
+              sector: 'Technology',
+              industry: 'Consumer Electronics',
+              isYahooFinance: true,
+            },
+            {
+              exchange: 'PNK',
+              shortname: 'APPLE RUSH COMPANY INC',
+              quoteType: 'EQUITY',
+              symbol: 'APRU',
+              index: 'quotes',
+              score: 20088,
+              typeDisp: 'Equity',
+              longname: 'Apple Rush Company, Inc.',
+              exchDisp: 'OTC Markets',
+              sector: 'Consumer Defensive',
+              industry: 'Beverages—Non-Alcoholic',
+              isYahooFinance: true,
+            },
+            {
+              index: 'd8c1b54896db4e148d5bff7bdd4778d0',
+              name: 'Apple Tree Partners',
+              permalink: 'apple-tree-partners',
+              isYahooFinance: false,
+            },
+            {
+              index: '902d61b27b7039fda61fc3a9dc44fe1a',
+              name: 'Applecart',
+              permalink: 'project-applecart',
+              isYahooFinance: false,
+            },
+          ],
+          news: [
+            {
+              uuid: '5a7c7c40-d411-32ea-8ac5-58c115fece40',
+              title:
+                "Why This May Be A 'Life Changing' Market Rally; Apple, Fed Meeting Loom As Tesla Run Hits 75%",
+              publisher: "Investor's Business Daily",
+              link: 'https://finance.yahoo.com/m/5a7c7c40-d411-32ea-8ac5-58c115fece40/why-this-may-be-a-%27life.html',
+              providerPublishTime: '2023-01-28T21:46:36.000Z',
+              type: 'STORY',
+              thumbnail: {
+                resolutions: [
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/ba6o5RYZCDuCe2zA7RimNg--~B/aD01NzI7dz0xMDEyO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/ibd.com/cc5c97ccaa42b69e95b7b7a633ba673c',
+                    width: 1012,
+                    height: 572,
+                    tag: 'original',
+                  },
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/OkfP.2TagBqMfvD4cXPoqg--~B/Zmk9ZmlsbDtoPTE0MDtweW9mZj0wO3c9MTQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/ibd.com/cc5c97ccaa42b69e95b7b7a633ba673c',
+                    width: 140,
+                    height: 140,
+                    tag: '140x140',
+                  },
+                ],
+              },
+              relatedTickers: [
+                'TSLA',
+                '^GSPC',
+                'COMP',
+                '^DJI',
+                '^RUT',
+                'ETSY',
+                'AAPL',
+                'AMZN',
+              ],
+            },
+            {
+              uuid: '543b5153-6709-428c-9a05-330d6d59322e',
+              title:
+                'Apple could limit WiFi 6E availability to iPhone 15 Pro models',
+              publisher: 'Engadget',
+              link: 'https://finance.yahoo.com/m/543b5153-6709-428c-9a05-330d6d59322e/apple-could-limit-wifi-6e.html',
+              providerPublishTime: '2023-01-28T19:36:40.000Z',
+              type: 'STORY',
+              thumbnail: {
+                resolutions: [
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/NVDuEHxiATI3A1pxIA_AgA--~B/aD0xMzMzO3c9MjAwMDthcHBpZD15dGFjaHlvbg--/https://s.yimg.com/os/creatr-uploaded-images/2022-09/6403bdc0-33d9-11ed-b6fd-41aceb29f6e3',
+                    width: 2000,
+                    height: 1333,
+                    tag: 'original',
+                  },
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/BF3egWLpEefkI03ajLziXQ--~B/Zmk9ZmlsbDtoPTE0MDtweW9mZj0wO3c9MTQwO2FwcGlkPXl0YWNoeW9u/https://s.yimg.com/os/creatr-uploaded-images/2022-09/6403bdc0-33d9-11ed-b6fd-41aceb29f6e3',
+                    width: 140,
+                    height: 140,
+                    tag: '140x140',
+                  },
+                ],
+              },
+            },
+            {
+              uuid: 'b794c14f-88eb-4a40-9c07-108b1b9b9a6a',
+              title:
+                'Apple will reportedly let anyone make apps for its mixed reality headset using Siri',
+              publisher: 'Engadget',
+              link: 'https://finance.yahoo.com/m/b794c14f-88eb-4a40-9c07-108b1b9b9a6a/apple-will-reportedly-let.html',
+              providerPublishTime: '2023-01-27T17:14:37.000Z',
+              type: 'STORY',
+              thumbnail: {
+                resolutions: [
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/SkEc1ToP9pWvaYLvfx8f0A--~B/aD0zMzM1O3c9NTAwMDthcHBpZD15dGFjaHlvbg--/https://s.yimg.com/os/creatr-uploaded-images/2021-03/f6458a50-7fdb-11eb-bffd-7dfe1212161c',
+                    width: 5000,
+                    height: 3335,
+                    tag: 'original',
+                  },
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/ELZCtoAFt4uJuz8hJu6.6Q--~B/Zmk9ZmlsbDtoPTE0MDtweW9mZj0wO3c9MTQwO2FwcGlkPXl0YWNoeW9u/https://s.yimg.com/os/creatr-uploaded-images/2021-03/f6458a50-7fdb-11eb-bffd-7dfe1212161c',
+                    width: 140,
+                    height: 140,
+                    tag: '140x140',
+                  },
+                ],
+              },
+            },
+            {
+              uuid: '35cf67f1-a69d-344e-ab98-86ca452e83b5',
+              title: 'Big Banks Try to Take On Apple Wallet and PayPal',
+              publisher: 'Motley Fool',
+              link: 'https://finance.yahoo.com/m/35cf67f1-a69d-344e-ab98-86ca452e83b5/big-banks-try-to-take-on.html',
+              providerPublishTime: '2023-01-28T12:00:00.000Z',
+              type: 'STORY',
+              thumbnail: {
+                resolutions: [
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/ADDzaOQxNo9Hh8wEZnZLgw--~B/aD0xMDgwO3c9MTkyMDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/motleyfool.com/4b16a7cd2a10202e9641652f1fb5896e',
+                    width: 1920,
+                    height: 1080,
+                    tag: 'original',
+                  },
+                  {
+                    url: 'https://s.yimg.com/uu/api/res/1.2/kNyRdA4CfvjTQNAOQlDT.w--~B/Zmk9ZmlsbDtoPTE0MDtweW9mZj0wO3c9MTQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/motleyfool.com/4b16a7cd2a10202e9641652f1fb5896e',
+                    width: 140,
+                    height: 140,
+                    tag: '140x140',
+                  },
+                ],
+              },
+              relatedTickers: ['PYPL', 'SPOT', 'FNKO', 'DIS'],
+            },
+          ],
+          nav: [],
+          lists: [],
+          researchReports: [],
+          screenerFieldResults: [],
+          totalTime: 133,
+          timeTakenForQuotes: 427,
+          timeTakenForNews: 900,
+          timeTakenForAlgowatchlist: 400,
+          timeTakenForPredefinedScreener: 400,
+          timeTakenForCrunchbase: 400,
+          timeTakenForNav: 400,
+          timeTakenForResearchReports: 0,
+          timeTakenForScreenerField: 0,
+          timeTakenForCulturalAssets: 0,
+        },
+      },
+    },
+  })
   @Get('search')
   async searchStockList(@Query('query') query) {
     const result = await this.stockService.searchStock(query);
